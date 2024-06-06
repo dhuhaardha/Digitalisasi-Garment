72094e868ffccda3ab9f4e8fb1d956c8d62a3d9a
dhuhaardha <dhuhaardha@gmail.com>
2024-06-06 15:28:57 +0700

    canvas for signature pad

--- try_canvas.php
+++ try_canvas.php
@@ -2,15 +2,16 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signature Pad</title>
-    <style>
-    canvas {
-        border: 1px solid #000;
-    }
-    </style>
+    <!-- Add Select2 CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+
+    <!-- Add jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!-- Add Select2 JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 </head>
+
 
 <body>
     <canvas id="canvas" width="300" height="150"></canvas>
@@ -22,6 +23,34 @@
     <script src="script_try_canvas.js">
 
     </script>
+
+<div class="row mb-3">
+    <label for="pengambilanDate" class="col-sm-2 col-form-label">Date Register</label>
+    <div class="col-sm-10">
+        <select class="form-control" name="person_office_recieved" id="security_select">
+            <?php
+            $sql = "SELECT tbls_nama FROM tb_list_security";
+            $result = mysqli_query($koneksi, $sql);
+
+            // Populate select options
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<option value='" . $row['tbls_nama'] . "''>" . $row['tbls_nama'] . "</option>";
+                }
+            } else {
+                echo "<option value=''>No security available</option>";
+            }
+            // Close database connection
+            ?>
+        </select>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('#security_select').select2();
+    });
+</script>
+
 </body>
 
 </html>
